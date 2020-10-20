8a16b1fb8bb098ebdfa9d337b2a1681d9592c17a
kumarkp <kumarreddy.kp@gmail.com>
2020-10-20 17:19:59 +0530

    Creating folder for faqs

--- src/Components/UIDashboard/index.jsx
+++ src/Components/UIDashboard/index.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import DBFaq from './DBFaqs/DBFaq';
+import DBAccountHistory from './DBAccountHistory/index';
+import DBVideos from './DBVideos/index';
+import './UIDashboard.css'
 
 export default function UIDashboard() {
     return (
         <div>
-            Home
+            <div className="row">
+                    <DBFaq/>
+                    <DBVideos/>
+            </div>
         </div>
     )
 }
